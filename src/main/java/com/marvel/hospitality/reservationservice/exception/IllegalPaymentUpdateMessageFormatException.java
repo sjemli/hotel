34156d025e54c6c05304a1b8cf062d54928240e8
sjemli <seifeddinejemli@gmail.com>
2026-01-29 23:33:38 +0200

    change2

--- src/main/java/com/marvel/hospitality/reservationservice/exception/IllegalPaymentUpdateMessageFormatException.java
+++ src/main/java/com/marvel/hospitality/reservationservice/exception/IllegalPaymentUpdateMessageFormatException.java
@@ -4,4 +4,8 @@ public class IllegalPaymentUpdateMessageFormatException extends RuntimeException
     public IllegalPaymentUpdateMessageFormatException(String message) {
         super(message);
     }
+
+    public IllegalPaymentUpdateMessageFormatException(String message, Throwable throwable) {
+        super(message, throwable);
+    }
 }
